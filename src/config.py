151e4ffb800496420768dc932212a24e6d013b84
Steven Ho <steven09ho@gmail.com>
2025-09-17 00:36:32 -0700

    added filters to take out drivers not currently on grid

--- src/config.py
+++ src/config.py
@@ -7,5 +7,26 @@ DATA_INTERIM = PROJECT_ROOT / "data_interim"
 MODELS_DIR = PROJECT_ROOT / "models"
 REPORTS_DIR = PROJECT_ROOT / "reports"
 
+# driver exlusion list
+EXCLUDED_DRIVER_NAMES = {
+  "Jack Doohan",
+  "Kevin Magnussen",
+  "Daniel Riccardo",
+  "Sergio Pérez",
+  "Valtteri Bottas",
+  "Logan Sargeant",
+  "Guanyu Zhou"
+}
+
+EXCLUDED_DRIVER_CODES = {
+  "DOO",
+  "MAG",
+  "RIC",
+  "PER",
+  "BOT",
+  "SAR",
+  "ZHO"
+}
+
 # Misc
 RANDOM_SEED = 42
